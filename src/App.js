7a3ec089b965d9be25d15a7d89b0c4112f8a20a7
vlad-js-love <vladk7072@gmail.com>
2025-03-13 21:25:43 +0200

    test CD

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <h1 style={{ fontSize: 70, color: "red" }}>value = {state}</h1>
       <button
-        style={{ background: "black", color: "white", padding: 15 }}
+        style={{ background: "black", color: "grey", padding: 15 }}
         onClick={increment}
       >
         incremernt
